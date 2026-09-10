d10b8ea6b37908c670439f13217797daa4351681
Ashraf Ali <imashraf824113@gmail.com>
2026-09-10 20:31:04 +0530

    Update assignment_03.py

--- assignment_03.py
+++ assignment_03.py
@@ -1,58 +1,102 @@
+# requirement
+# 1. A car  (name, speedLimit, start(), stop(), speedUP(), speedDown(), showSpeed())
+# 2. user can start the car 
+# 3. user can see Speed,
+# 4. can change speed (speedUp 10+, speedDown 10-)
+# 5. can stop the car.
+
+
+
+
 class Car:
-
-    def __init__(self, name, speedLimit):
-        self.name = name
-        self.speedLimit = speedLimit
-        self.speed = 0
-        self.isStarted = False
-
+    carBrand = ""
+    speedLimit = 0
+    currentSpeed = 0
+    isStarted = False
+  
+    def dashboard(self):
+        print(f"-->You are in {self.carBrand}<--")
+        print(f"-->Speed limit = {self.speedLimit}km/h<---")
+        print(f"-->Current speed = {self.currentSpeed}km/h<---")   
+    
     def start(self):
-        self.isStarted = True
-        print(self.name, "started.")
+        if self.isStarted:
+            print(f"{self.carBrand} is already started.")
+         
+        if not self.isStarted:
+            self.isStarted = True
+            print(f"{self.carBrand} is started!")
+        self.dashboard()   
+   
+    def speedUp(self):
+        if self.isStarted:
+            self.currentSpeed += 10
+            if self.currentSpeed > self.speedLimit:
+                self.currentSpeed -= 10
+                print("you are at max speed.")
+                
+        if not self.isStarted:
+            print(f"Start {self.carBrand} car first!")
+        self.dashboard()
+        
+    def speedDown(self):
+        if self.isStarted:
+            self.currentSpeed -= 10
+            if self.currentSpeed < 0:
+                self.currentSpeed += 10
+                print("you are at min speed.")
+                
+        if not self.isStarted:
+            print(f"Start {self.carBrand} carf first!")
+        self.dashboard()
 
     def stop(self):
-        self.speed = 0
-        self.isStarted = False
-        print(self.name, "stopped.")
-
-    def speedUp(self):
+        if self.isStarted:
+            self.currentSpeed = 0
+            self.isStarted = False
+            print(f"{self.carBrand} is Stopped!")
         if not self.isStarted:
-            print("Please start the car first.")
-        elif self.speed + 10 <= self.speedLimit:
-            self.speed += 10
-            print("Speed increased to:", self.speed, "km/h")
-        else:
-            print("Cannot exceed speed limit.")
+            print(f"{self.carBrand} is already Stopped!")
+        self.dashboard()
 
-    def speedDown(self):
-        if not self.isStarted:
-            print("Please start the car first.")
-        elif self.speed >= 10:
-            self.speed -= 10
-            print("Speed decreased to:", self.speed, "km/h")
-        else:
-            print("Car is already at 0 km/h.")
+        
+# prepare a new car.
+Toyota = Car()
+Toyota.carBrand = "Toyota"
+Toyota.speedLimit = 300
 
-    def showSpeed(self):
-        print("Current Speed:", self.speed, "km/h")
+Hundayi = Car()
+Hundayi.carBrand = "Hundayi"
+Hundayi.speedLimit = 100
+
+Hundayi = Car()
+Hundayi.carBrand = "Hundayi"
+Hundayi.speedLimit = 100
 
 
-# Creating object
-car = Car("BMW", 100)
+# *********
 
-# Using methods
-car.start()
-
-car.showSpeed()
-
-car.speedUp()
-car.speedUp()
-car.speedUp()
-
-car.showSpeed()
-
-car.speedDown()
-
-car.showSpeed()
-
-car.stop()
+while True:
+    print("---------------")
+    print("1. Start ")
+    print("2. speedUp by +10")
+    print("3. speedDown  by -10")
+    print("4. stop ")
+    print("5. exit ")
+    print("---------------")
+    choice = int(input("Choose an option: "))
+    print(f"\nYou have choosen {choice}\n")
+    
+    if choice == 1:
+        Hundayi.start()
+        
+    elif choice == 2:
+        Hundayi.speedUp()
+        
+    elif choice == 3:
+         Hundayi.speedDown()
+        
+    elif choice == 4:
+        Hundayi.stop()
+    elif choice == 5:
+        break
